00e988b0293a498d87c07da4051164aa41a6d7cc
Brendan Hellar and Tessa Sullivan <tessa@>
2019-03-28 15:10:56 -0700

    add the option to select a favorite animal as well as adding more image variations

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     var age = parseInt($("#age").val());
     var sex = $("#sex").val();
     var orientation = $("#orientation").val();
+    var favoriteAnimal = $("#favoriteAnimal").val();
     var match;
     var matchImage;
 
@@ -12,32 +13,73 @@ $(document).ready(function() {
         // Harry Potter characters
         if ((orientation === 'Straight' && sex === 'Male')
           || (orientation === 'Gay' && sex === 'Female')) {
-            match = 'Hermione Granger';
-            matchImage = 'img/HermioneGranger.jpg';
+            if (favoriteAnimal === "Cat") {
+              match = 'Hermione Granger';
+              matchImage = 'img/HermioneGranger.jpg';
+            } else if (favoriteAnimal === "Dog") {
+              match = 'Hannah Abbott';
+              matchImage = 'img/HannahAbbott.jpg';
+            } else if (favoriteAnimal === "Snake") {
+              match = 'Bellatrix Lestrange';
+              matchImage = 'img/BellatrixLestrange.jpg';
+            } else if (favoriteAnimal === "Bird") {
+              match = 'Fleur Delacour';
+              matchImage = 'img/FleurDelacour.jpg';
+            }
           } else if ((orientation === 'Gay' && sex === 'Male')
           || (orientation === 'Straight' && sex === 'Female')){
-            match = 'Harry Potter';
-            matchImage = 'img/HarryPotter.jpg';
-
+            if (favoriteAnimal === "Cat") {
+              match = 'Harry Potter';
+              matchImage = 'img/HarryPotter.jpg';
+            } else if (favoriteAnimal === "Dog") {
+              match = 'Sirius Black';
+              matchImage = 'img/SiriusBlack.jpg';
+            } else if (favoriteAnimal === "Snake") {
+              match = 'Draco Malfoy';
+              matchImage = 'img/DracoMalfoy.jpg';
+            } else if (favoriteAnimal === "Bird") {
+              match = 'Ron Weasley';
+              matchImage = 'img/RonWeasley.jpg';
           }
+        }
       } else {
         //Game of Thrones characters
         if ((orientation === 'Straight' && sex === 'Male')
           || (orientation === 'Gay' && sex === 'Female')) {
-            match = 'Daenerys Targaryen';
-            matchImage = 'img/DaenerysTargaryen.jpg';
-
+            if (favoriteAnimal === "Cat") {
+              match = 'Daenerys Targaryen';
+              matchImage = 'img/DaenerysTargaryen.jpg';
+            } else if (favoriteAnimal === "Dog") {
+              match = 'Brienne of Tarth';
+              matchImage = 'img/Brienne.jpg';
+            } else if (favoriteAnimal === "Snake") {
+              match = 'Melisandre';
+              matchImage = 'img/Melisandre.jpg';
+            } else if (favoriteAnimal === "Bird") {
+              match = 'Missandei';
+              matchImage = "img/Missandei.jpg";
+            }
           } else if ((orientation === 'Gay' && sex === 'Male')
           || (orientation === 'Straight' && sex === 'Female')){
-            match = 'Khal Drogo';
-            matchImage = 'img/KhalDrogo.jpg';
-
+            if (favoriteAnimal === "Cat") {
+              match = 'Khal Drogo';
+              matchImage = 'img/KhalDrogo.jpg';
+            } else if (favoriteAnimal === "Dog") {
+              match = 'John Snow';
+              matchImage = 'img/Snow.jpg';
+            } else if (favoriteAnimal === "Snake") {
+              match = 'Ramsey Bolton';
+              matchImage = 'img/Bolton.png';
+            } else if (favoriteAnimal === "Bird") {
+              match = 'Tyrion';
+              matchImage = "img/Tyrion.jpeg";
+            }
           }
           // alert("match is " + match);
       }
-        $("#quiz").hide();
-       $("#match").prepend("<h1>Your match is " + match + "</h1>");
-       $("#match").append("<img src=" + matchImage + ">");
+      $("#quiz").hide();
+      $("#match").prepend("<h1>Your match is " + match + "</h1>");
+      $("#match").append("<img src=" + matchImage + ">");
     } else {
       alert("Please enter your age");
     }
